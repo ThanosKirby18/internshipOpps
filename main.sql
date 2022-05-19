f29bf4a170da09e07ef936a9afc809bdf75976aa
Emiliano Melendrez <77299569+ThanosKirby18@users.noreply.github.com>
2022-05-19 15:56:00 +0000

    aDDED SPACE X INTERNSHIP OPPORTIUNITE

--- main.sql
+++ main.sql
@@ -2,10 +2,13 @@ CREATE TABLE internshipOpps
 (
   Title TEXT,
   deadline DATE,
-  Duration INT,
+  DurationWeeks INT,
   Description TEXT,
   paid BOOLEAN
 );
 INSERT INTO internshipOpps values
-
+  ('Summer 2022 Software Engineering Internship/Co-op SAVE SPACEX', '2022', 12, 'SpaceX was founded under the belief that a future where humanity is out exploring the stars is fundamentally more exciting than one where we are not. We are developing the technologies to make this possible, with the ultimate goal of enabling human life on Mars.', true);
+.print
+.print 'Possible Internship'
+SELECT * FROM internshipOpps
 
